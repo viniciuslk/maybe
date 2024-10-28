d69adedb6e0d4765dc98802a8135a574424668dc
Josh Pigford <josh@joshpigford.com>
2024-10-28 10:32:10 -0400

    Providable set up

--- app/models/concerns/providable.rb
+++ app/models/concerns/providable.rb
@@ -7,7 +7,11 @@ module Providable
 
   class_methods do
     def security_prices_provider
-      synth_provider
+      if ENV["MARKETSTACK_API_KEY"].present?
+        marketstack_provider
+      elsif ENV["SYNTH_API_KEY"].present?
+        synth_provider
+      end
     end
 
     def exchange_rates_provider
@@ -21,6 +25,11 @@ module Providable
     def synth_provider
       api_key = self_hosted? ? Setting.synth_api_key : ENV["SYNTH_API_KEY"]
       api_key.present? ? Provider::Synth.new(api_key) : nil
+    end
+
+    def marketstack_provider
+      api_key = ENV["MARKETSTACK_API_KEY"]
+      api_key.present? ? Provider::Marketstack.new(api_key) : nil
     end
 
     private
